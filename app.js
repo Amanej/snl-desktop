da707767ddf6d04e532c359e00117eb5763ebb72
Alexander Alemayhu <alexander@alemayhu.com>
2016-01-16 19:27:41 +0100

    more renamings for app and data

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-var app = angular.module('snlApp', ['ngSanitize']);
-app.controller('searchCtrl', function($scope, $http) {
+var snlApp = angular.module('snlApp', ['ngSanitize']);
+snlApp.controller('searchCtrl', function($scope, $http) {
     $scope.searchme;
     $scope.fetchResults = function() {
     $http({
@@ -7,7 +7,7 @@ app.controller('searchCtrl', function($scope, $http) {
         url: 'https://snl.no/api/v1/search?query=' + $scope.searchme
       }).then(function successCallback(response){
         console.log("It works");
-        $scope.myData = response.data;
+        $scope.articles = response.data;
       }, function errorCallback(response) {
         console.log("its alive");
       });
